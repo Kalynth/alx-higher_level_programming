dc442687258454680ea062c99d5bf61f1aeb01b5
Kalynth <annmwangi.w@gmail.com>
2024-02-26 03:33:32 -0800

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(97, 122 + 1):
-    if chr(i) == 'e' or  chr(i) == 'q':
+    if chr(i) == 'e' or chr(i) == 'q':
         continue
     print(chr(i).format(i), end="")
