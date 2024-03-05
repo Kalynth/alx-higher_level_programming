52d9015c094f98c99c9b3d1509cb9fa5c4b73104
Kalynth <annmwangi.w@gmail.com>
2024-03-05 03:58:46 -0800

    0x06. Python - Classes and Objects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+class Square:
+     def __init__(self, size):
+        """Constructor.
+        Args:
+            size: length of side of the square.
+        """
+        self.__size = size
